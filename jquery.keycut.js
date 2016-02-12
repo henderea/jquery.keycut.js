51d825a83b619df93acdc8ccae1d53635a888235
Eric Henderson <henderea@gmail.com>
2016-02-12 15:26:36 -0500

    make some progress

---
+++ jquery.keycut.js
@@ -0,0 +1,12 @@
+(function($){
+    function KeyCut(obj, options) {
+    }
+    function KeyCutManager(obj) {
+        this.obj = obj
+    }
+    $.fn.keycut = function(name, options) {
+        this.each(function(obj) {
+
+        });
+    }
+})(jQuery);
